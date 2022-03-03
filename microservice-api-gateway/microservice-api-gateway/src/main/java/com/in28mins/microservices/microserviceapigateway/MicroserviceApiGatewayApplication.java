8b181bf7ae0f44a298b5147e0148d7479458d6ad
vikash <91241721+vikMaxKr@users.noreply.github.com>
2022-03-03 11:42:58 +0530

    Add files via upload

---
+++ microservice-api-gateway/microservice-api-gateway/src/main/java/com/in28mins/microservices/microserviceapigateway/MicroserviceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.in28mins.microservices.microserviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApiGatewayApplication.class, args);
+	}
+
+}
---
+++ microservice-api-gateway/microservice-api-gateway/src/test/java/com/in28mins/microservices/microserviceapigateway/MicroserviceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28mins.microservices.microserviceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
